20715e0942bed3dfffe06b839f976c9317fc087d
AI-CREATOR-UZ <147493641+AI-CREATOR-UZ@users.noreply.github.com>
2023-10-20 15:06:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+# Define the login credentials
+LOGIN_USERNAME = "men"
+LOGIN_PASSWORD = "men"
+
+# Function to authenticate the user
+def authenticate(username, password):
+    return username == LOGIN_USERNAME and password == LOGIN_PASSWORD
+
+# Function to get PDF text
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -18,7 +27,7 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-
+# Function to get text chunks
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
@@ -29,18 +38,15 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
+# Function to create a vector store
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
+# Function to create a conversation chain
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -50,7 +56,7 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-
+# Function to handle user input
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
@@ -63,42 +69,50 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
-
+# Streamlit UI
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    # Check if the user is authenticated
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
 
-    st.header("Yuklagan PDF fayllaringiz bilan suhbat :books:")
-    user_question = st.text_input("Fayllaringizga aloqador savol:")
-    if user_question:
-        handle_userinput(user_question)
+    if not st.session_state.authenticated:
+        st.header("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        if st.button("Login"):
+            if authenticate(username, password):
+                st.session_state.authenticated = True
+            else:
+                st.error("Invalid username or password.")
 
-    with st.sidebar:
-        st.subheader("Sizning fayllaringiz")
-        pdf_docs = st.file_uploader(
-            "Fayllaringizni yuklang va 'Process' tugmasini bosing", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Tahlil qilyapman"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    if st.session_state.authenticated:
+        st.header("Chat with multiple PDFs :books:")
+        user_question = st.text_input("Ask a question related to your documents:")
+        if user_question:
+            handle_userinput(user_question)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+        with st.sidebar:
+            st.subheader("Your Files")
+            pdf_docs = st.file_uploader(
+                "Upload your files and click the 'Process' button", accept_multiple_files=True)
+            if st.button("Process"):
+                with st.spinner("Analyzing..."):
+                    # Get PDF text
+                    raw_text = get_pdf_text(pdf_docs)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+                    # Get text chunks
+                    text_chunks = get_text_chunks(raw_text)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+                    # Create a vector store
+                    vectorstore = get_vectorstore(text_chunks)
 
+                    # Create a conversation chain
+                    st.session_state.conversation = get_conversation_chain(
+                        vectorstore)
 
 if __name__ == '__main__':
     main()
